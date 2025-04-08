c3fb3be4c3d30174cd13576823bb5fa7917d1642
DamjannM <damjanmilosavljevic@gmail.com>
2025-04-08 19:02:35 +0200

    added landing page with image slider and additional info

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const header = document.querySelector(".header");
 const aboutUs = document.querySelector("#about__us");
 const nav = document.querySelector(".nav");
 const navHeight = nav.getBoundingClientRect().height;
-const googlemap = document.querySelector("gmp-map");
+const googleMap = document.querySelector("gmp-map");
 const form = document.getElementById("form");
 const dataName = document.querySelector("#data-name");
 const dataLastName = document.querySelector("#data-lastname");
@@ -14,6 +14,17 @@ const checkOutInput = document.querySelector("#dayout");
 const currentDate = document.querySelector(".current-date");
 const daysTag = document.querySelector(".days");
 const prevNextIcon = document.querySelectorAll(".icons span");
+const logoutBtn = document.querySelector(".logout__btn");
+const loginBtn = document.querySelector(".login__btn");
+const inputAdmin = document.querySelector("#admin");
+const inputPin = document.querySelector("#pin");
+const login = document.querySelector("#login");
+
+const admin1 = {
+  owner: "Vladimir Blagoev",
+  username: "VB",
+  pin: 1234,
+};
 
 const months = [
   "January",
@@ -38,8 +49,8 @@ let clientData = [];
 let selectedDate = null;
 let bookedArr = [];
 let isSelectedCheckIn = true;
-let twoLetterCheckInInputValue = checkInInput.value.substring(0, 2);
-
+let twoLetterCheckInInputValue;
+let twoLetterMonthInput;
 //STICKY NAVIGATION BAR
 const stickyNav = function (entries) {
   const [entry] = entries;
@@ -84,19 +95,44 @@ const addListener = function () {
 
           checkInInput.value = `${selectedDate}-${currMonth + 1}-${currYear}`;
           checkOutInput.value = "";
+          let indexOfFirstSlash = checkInInput.value.indexOf("-");
+          let indexOfSecondSlash = checkInInput.value.indexOf("-", 3);
+
           twoLetterCheckInInputValue = checkInInput.value.substring(0, 2);
+          twoLetterMonthInput = checkInInput.value.substring(
+            indexOfFirstSlash + 1,
+            indexOfSecondSlash
+          );
+          //CLOSEST BOOKED DAY CLICKABLE AFTER CLICKING CHECKIN
+          let nextElement = document.querySelectorAll(".booked");
+
+          let nextBookedDay = Array.from(nextElement).find(
+            (day) => +day.textContent > +selectedDate
+          );
+          //PREVENTION WHEN THERE IS NO BOOKED DAY AS NEXT ELEMENT
+          if (nextBookedDay !== null && nextBookedDay !== undefined) {
+            nextBookedDay.style.pointerEvents = "auto";
+            nextBookedDay.style.cursor = "pointer";
+          }
+
+          // IF CHECKIN DAY IS 1 DIGIT NUMBER REMOVE -
           if (twoLetterCheckInInputValue.includes("-")) {
             twoLetterCheckInInputValue = twoLetterCheckInInputValue.substring(
               0,
               1
             );
           }
-          console.log(twoLetterCheckInInputValue);
           isSelectedCheckIn = false;
         } else if (!isSelectedCheckIn) {
-          console.log(twoLetterCheckInInputValue);
-          console.log(selectedDate);
-          if (+selectedDate > +twoLetterCheckInInputValue) {
+          console.log(currMonth + 1, twoLetterMonthInput);
+
+          if (currMonth + 1 > +twoLetterMonthInput) {
+            checkOutInput.value = `${selectedDate}-${
+              currMonth + 1
+            }-${currYear}`;
+            this.classList.add("checkin");
+            isSelectedCheckIn = true;
+          } else if (+selectedDate > +twoLetterCheckInInputValue) {
             checkOutInput.value = `${selectedDate}-${
               currMonth + 1
             }-${currYear}`;
@@ -280,9 +316,128 @@ form.addEventListener("submit", function (e) {
 //--------------------------\
 
 // Event listener for map
-googlemap.addEventListener("click", () => {
+googleMap.addEventListener("click", () => {
   window.open("https://maps.app.goo.gl/GVjUfC6sCryhJA4e6");
 });
 
-console.log(bookedArr);
-console.log(clientData);
+// console.log(bookedArr);
+// console.log(clientData);
+
+//LOGIN FUNCTIONALITY
+
+function loginFunction() {
+  inputAdmin.value = "";
+  inputPin.value = "";
+  admin = true;
+  inputAdmin.classList.add("hidden");
+  inputPin.classList.add("hidden");
+  loginBtn.classList.add("hidden");
+  form.classList.add("hidden");
+  logoutBtn.classList.remove("hidden");
+  renderCalendar();
+}
+loginBtn.addEventListener("click", () => {
+  if (inputAdmin.value == admin1.username && inputPin.value == admin1.pin)
+    loginFunction();
+});
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    if (inputAdmin.value == admin1.username && inputPin.value == admin1.pin)
+      loginFunction();
+  }
+});
+
+logoutBtn.addEventListener("click", () => {
+  admin = false;
+  inputAdmin.classList.remove("hidden");
+  inputPin.classList.remove("hidden");
+  loginBtn.classList.remove("hidden");
+  form.classList.remove("hidden");
+  logoutBtn.classList.add("hidden");
+  renderCalendar();
+});
+
+//PHOTO SLIDER
+const slider = function () {
+  const slides = document.querySelectorAll(".slide");
+  const btnLeft = document.querySelector(".slider__btn--left");
+  const btnRight = document.querySelector(".slider__btn--right");
+  const dotContainer = document.querySelector(".dots");
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  // Functions
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll(".dots__dot")
+      .forEach((dot) => dot.classList.remove("dots__dot--active"));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add("dots__dot--active");
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  // Next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    createDots();
+
+    activateDot(0);
+  };
+  init();
+
+  // Event handlers
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") prevSlide();
+    e.key === "ArrowRight" && nextSlide();
+  });
+
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots__dot")) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    }
+  });
+};
+slider();
